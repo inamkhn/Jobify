684d278f5a37f4434fa08e6e3306b8c37c06696b
inamkhn <inamkhn7786@gmail.com>
2024-02-29 23:37:56 -0800

    first commit

---
+++ src/app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,12 @@
+import CreateJobForm from '@/components/CreateJobForm'
+import React from 'react'
+
+const AddJob = () => {
+  return (
+    <div>
+      <CreateJobForm/>
+    </div>
+  )
+}
+
+export default AddJob
